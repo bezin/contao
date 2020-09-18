2e138eea0c517abeb77365bfed6cabe825dd5ff7
Benedict Massolle <bm@presentprogressive.de>
2020-09-18 19:17:00 +0200

    fix cs

--- core-bundle/tests/ContaoCoreBundleTest.php
+++ core-bundle/tests/ContaoCoreBundleTest.php
@@ -39,7 +39,6 @@ use Contao\CoreBundle\Event\PreviewUrlCreateEvent;
 use Contao\CoreBundle\Event\RobotsTxtEvent;
 use Contao\CoreBundle\Event\SlugValidCharactersEvent;
 use Contao\CoreBundle\Event\UndoDescriptionEvent;
-use Contao\CoreBundle\EventListener\Undo\UndoDescriptionListener;
 use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
 use Symfony\Cmf\Component\Routing\DependencyInjection\Compiler\RegisterRouteEnhancersPass;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -100,7 +99,7 @@ class ContaoCoreBundleTest extends TestCase
                             PreviewUrlConvertEvent::class => ContaoCoreEvents::PREVIEW_URL_CONVERT,
                             RobotsTxtEvent::class => ContaoCoreEvents::ROBOTS_TXT,
                             SlugValidCharactersEvent::class => ContaoCoreEvents::SLUG_VALID_CHARACTERS,
-                            UndoDescriptionEvent::class => ContaoCoreEvents::UNDO_DESCRIPTION
+                            UndoDescriptionEvent::class => ContaoCoreEvents::UNDO_DESCRIPTION,
                         ];
 
                         $this->assertEquals(new AddEventAliasesPass($eventAliases), $pass);
